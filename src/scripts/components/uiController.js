594adfcf9a4b9eb312df96c978ffd9a311e409a8
Joey Leger <legerinvestments@gmail.com>
2018-11-18 18:22:46 -0500

    [FIX] Fixed the menu and shrunk logo

--- src/scripts/components/uiController.js
+++ src/scripts/components/uiController.js
@@ -9,17 +9,17 @@ const uiController = (function() {
 
     // Set up some variables
     const menuButton = document.querySelector('.nav__menu');
-    const navMenu = document.querySelector('.header__container');
+    const navMenu = document.querySelector('.header__links');
     const hamburger = document.querySelector('.hamburger');
     
     // Toggles the menu
     const toggleMenu = () => {
-        if (navMenu.classList.contains('header__container--show')) {
-            navMenu.classList.remove('header__container--show');
+        if (navMenu.classList.contains('header__links--show')) {
+            navMenu.classList.remove('header__links--show');
             hamburger.classList.remove('fa-times');
             hamburger.classList.add('fa-bars');
         } else {
-            navMenu.classList.add('header__container--show');
+            navMenu.classList.add('header__links--show');
             hamburger.classList.remove('fa-bars');
             hamburger.classList.add('fa-times');
         }
