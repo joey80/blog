36eb5ebfa313850c9bf43fb6ba0f1fa311f1986e
Joey Leger <legerinvestments@gmail.com>
2018-12-12 13:30:27 -0500

    [UPDATE] Updated index to php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    $test = 'test';
+?>
 <!doctype html>
 <html class="no-js" lang="en">
     <head>
@@ -55,7 +58,7 @@
         <div class="container container--white container--bump">
             <section class="content">
                 <div class="content__container">
-                    <h1>Hello World</h1>
+                    <h1><?php echo $test ?></h1>
                     <p>Lorem ipsum dolor sit amet, mea virtute eruditi ad. Pri ea nostro oportere, te eos ornatus assentior.
                         No malorum nominati mel, luptatum deserunt posidonium sea te, eam id inani denique. Falli zril
                         definitionem cu per. Cu prima impetus tacimates vis, dicat volutpat sit ut. Odio nominati mei ad.</p>
