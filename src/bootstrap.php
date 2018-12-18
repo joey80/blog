e7d7f4fd9bcb89ffdca74cc521cc7bd08368d0a4
Joey Leger <legerinvestments@gmail.com>
2018-12-17 20:45:57 -0500

    latest build

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
     // Load libraries
-    require_once './libraries/core.php';
-    require_once './libraries/controller.php';
-    require_once './libraries/catabase.php';
+    require_once 'libraries/Core.php';
+    require_once 'libraries/Controller.php';
+    require_once 'libraries/Database.php';
