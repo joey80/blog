222539abb2e36908f81b0c9c574fe08fc4967578
Joey Leger <legerinvestments@gmail.com>
2018-12-21 12:14:02 -0500

    latest build

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once '../src/libraries/Core.php';
-    require_once 'libraries/Controller.php';
+    require_once '../src/libraries/Controller.php';
     //require_once 'libraries/Database.php';
 
     // Init core library
