b4223f6346fa274c0253c5fe8c965d755917bc01
Joey Leger <legerinvestments@gmail.com>
2018-12-21 11:09:46 -0500

    [ADD] Tracking htaccess

--- public/index2.php
+++ public/index2.php
@@ -8,7 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="description" content="Blog CMS developed with PHP created by Joey Leger">
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
-        <title>blogStorn</title>
+        <title>blogStorn 2.0</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Raleway|Sansita">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
