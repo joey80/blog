653535a30006c17f8942122adc845719b69b0442
Joey Leger <legerinvestments@gmail.com>
2018-12-21 14:56:42 -0500

    latest build

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
     echo 'hello from index ';
     
     require_once '../src/libraries/Core.php';
-    require_once '../src/libraries/Controller.php';
+    //require_once '../src/libraries/Controller.php';
 
     // Init core library
     $init = new Core();
