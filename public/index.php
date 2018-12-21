2c0b544a869202ca77860929389ab95239ed5500
Joey Leger <legerinvestments@gmail.com>
2018-12-21 11:41:21 -0500

    latest build

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
     //require_once '../src/bootstrap.php';
-    require_once '../src/libraries/Core.php';
+    include('../src/libraries/Core.php');
 
     // Init core library
     $init = new Core();
