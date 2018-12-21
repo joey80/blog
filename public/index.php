dbbd204d124033ce30635629827366be68d9844a
Joey Leger <legerinvestments@gmail.com>
2018-12-21 14:44:33 -0500

    latest build

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
     //require_once '../src/bootstrap.php';
     require_once '../src/libraries/Core.php';
-    require_once '../src/libraries/Controller.php';
+    //require_once '../src/libraries/Controller.php';
 
     // Init core library
     $init = new Core();
