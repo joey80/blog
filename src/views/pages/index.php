95c3fed75b3ee99f050641fdecb69fff244b2965
Joey Leger <legerinvestments@gmail.com>
2019-01-08 20:08:15 -0500

    [ADD] Adjusted color and weight to link styles

--- src/views/pages/index.php
+++ src/views/pages/index.php
@@ -7,7 +7,7 @@
                 <div class="content__container" role="main">
                     <h1><?php echo $test ?></h1>
                     <div id="map"></div>
-                    <p>Lorem ipsum dolor sit amet, mea virtute eruditi ad. Pri ea nostro oportere, te eos ornatus assentior.
+                    <p>Lorem ipsum dolor sit amet, <a href="#">mea virtute eruditi ad</a>. Pri ea nostro oportere, te eos ornatus assentior.
                         No malorum nominati mel, luptatum deserunt posidonium sea te, eam id inani denique. Falli zril
                         definitionem cu per. Cu prima impetus tacimates vis, dicat volutpat sit ut. Odio nominati mei ad.</p>
                     <h2>Hello World</h2>
